0316b075fcbe49867a34a356a36110342bd855fe
eaculb <eculbertson@butterflynetinc.com>
2021-01-18 00:59:31 -0500

    css -> scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
